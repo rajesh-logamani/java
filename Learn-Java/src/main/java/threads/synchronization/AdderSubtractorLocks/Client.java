5aee95d2d0a37518bc7e2dab1fcdd77d69c28409
Rajesh Logamani <rajeshlogamani@gmail.com>
2025-06-14 23:41:01 +0530

    Synchronization || Locks

---
+++ Learn-Java/src/main/java/threads/synchronization/AdderSubtractorLocks/Client.java
@@ -0,0 +1,31 @@
+package threads.synchronization.AdderSubtractorLocks;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class Client {
+    public static void main(String[] args) {
+        Value value = new Value(0);
+        Lock lock = new ReentrantLock();
+
+        Adder adder = new Adder(value, lock);
+        Subtractor subtractor = new Subtractor(value, lock);
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        try {
+            Future<Void> add = executorService.submit(adder);
+            Future<Void> sub = executorService.submit(subtractor);
+
+            add.get();
+            sub.get();
+            System.out.println(value.getValue());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        finally {
+            executorService.shutdown();
+        }
+    }
+}
