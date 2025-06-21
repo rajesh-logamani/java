a6aeefe0274c7a764e45f2b2d58b7b7c857b649d
Rajesh Logamani <rajeshlogamani@gmail.com>
2025-06-21 21:21:12 +0530

    Threads || Producer Consumer

---
+++ Learn-Java/src/main/java/threads/producerCounsumer/withProblem/Client.java
@@ -0,0 +1,19 @@
+package threads.producerCounsumer.withProblem;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class Client {
+
+    public static void main(String[] args) {
+        Store store = new Store();
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        for(int i = 0 ; i < 100; i++){
+            executorService.execute(new Producer(store));
+        }
+
+        for(int i = 0 ; i < 100; i++){
+            executorService.execute(new Consumer(store));
+        }
+    }
+}
---
+++ Learn-Java/src/main/java/threads/producerCounsumer/withProblem/Consumer.java
@@ -0,0 +1,17 @@
+package threads.producerCounsumer.withProblem;
+
+public class Consumer implements Runnable {
+    private Store store;
+
+    public Consumer(Store store) {
+        this.store = store;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            if (store.getItems().size() > 0)
+                store.remove();
+        }
+    }
+}
